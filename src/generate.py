7c4bf685742e5807dd95c4f9633fb153c904dd3e
riyadhrazzaq <riyadh.razzaq@gmail.com>
2024-05-05 21:01:13 +0200

    modify generator for nucleus batch support

--- src/generate.py
+++ src/generate.py
@@ -5,6 +5,7 @@ import random
 
 import numpy as np
 import torchtext
+
 try:
     torchtext.disable_torchtext_deprecation_warning()
 except:
@@ -22,7 +23,6 @@ random.seed(0)
 np.random.seed(0)
 torch.manual_seed(0)
 
-
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s %(levelname)s %(module)s - %(funcName)s: %(message)s',
@@ -36,7 +36,9 @@ args.add_argument('srcfile', type=str)
 args.add_argument('tgtfile', type=str)
 args.add_argument('checkpoint_dir', type=str)
 args.add_argument('--outfile', type=str, help="save generated output to this file")
-args.add_argument('--search', choices=["greedy", "beam", "nucleus"], type=str, default="greedy", help="search method to use")
+args.add_argument('--search', choices=["greedy", "beam", "nucleus"], type=str, default="greedy",
+                  help="search method to use")
+args.add_argument('--p', type=float, default=0.9, help="nucleus sampling p value")
 
 args = args.parse_args()
 
@@ -62,15 +64,7 @@ def main():
     with open(args.tgtfile) as tgtfile:
         targets = tgtfile.readlines()
 
-    hypotheses = []
-
-    for i, source in enumerate(sources):
-        hyp, _ = generate(model, source, src_tokenizer, tgt_tokenizer, cfg, method=args.search)
-        hypotheses.append(hyp[0])
-
-        if i % 100 == 0:
-            logger.info(f"Generated {i}/{len(sources)} hypotheses")
-
+    hypotheses = generate_hypotheses(sources, model, src_tokenizer, tgt_tokenizer, cfg, args)
     metrics = compute_metrics(hypotheses, targets)
     metrics = {k: v * 100 for k, v in metrics.items()}
     pprint.pp(metrics)
@@ -78,6 +72,32 @@ def main():
     if args.outfile:
         with open(args.outfile, "w") as f:
             f.writelines([h + '\n' for h in hypotheses])
+
+
+def generate_hypotheses(sources, model, src_tokenizer, tgt_tokenizer, cfg, args):
+    hypotheses = []
+    # generate samplewise
+    if args.search in ["beam", "greedy"]:
+        for i, source in enumerate(sources):
+            hyp, _ = generate(model, source, src_tokenizer, tgt_tokenizer, cfg, method=args.search)
+            hypotheses.append(hyp[0])
+
+            if i % 100 == 0:
+                print(f"Generated {i}/{len(sources)} hypotheses", end='\r')
+
+    # generate batchwise
+    elif args.search == "nucleus":
+        assert args.p is not None and 1.0 > args.p > 0.0, "p must be in (0, 1)"
+        batch_size = 64
+        i = 0
+        while i < len(sources):
+            hyp, _ = generate(model, sources[i: i + batch_size], src_tokenizer, tgt_tokenizer, cfg, method="nucleus",
+                              p=args.p)
+            hypotheses.extend(hyp)
+            i += batch_size
+            print(f"Generated {i}/{len(sources)} hypotheses", end='\r')
+
+    return hypotheses
 
 
 if __name__ == '__main__':
