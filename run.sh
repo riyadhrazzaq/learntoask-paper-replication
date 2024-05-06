7e1b0942e60d42b8b37053faac982387e706d877
riyadhrazzaq <riyadh.razzaq@gmail.com>
2024-05-06 20:12:16 +0200

    modify run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,8 @@ srun --container-image=/enroot/nvcr.io_nvidia_pytorch_23.12-py3.sqsh \
     --container-workdir="`pwd`" \
     --container-mounts=/netscratch/$USER:/netscratch/$USER,/ds:/ds:ro,"`pwd`":"`pwd`" \
     --partition=RTX3090 \
+    --mem 30GB \
     --gpus=1 \
     --task-prolog="`pwd`/install.sh" \
-    --pty /bin/bash
+    python src/generate.py data/test.src data/test.tgt $1 --search $2 --p $3 --outfile $1/test.tgt.$2
 
